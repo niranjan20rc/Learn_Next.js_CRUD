4f82d48441eba7cca4f162f6af39412f6130dbb0
niranjan20rc <niranjanrc20@gmail.com>
2025-09-16 23:04:39 +0530

    edited on db link

--- app/src/lib/mongodb.js
+++ app/src/lib/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 
-const MONGO_URI="mongodb://localhost:27017/testdb";
+const MONGO_URI="mongodb+srv://niranjancse2023:niranjanzod@cluster0.o5clepr.mongodb.net/myDatabase?retryWrites=true&w=majority&appName=Cluster0";
 
 export async function connectDB(){
     if(mongoose.connection.readyState===1 ||mongoose.connection.readyState===2)return
